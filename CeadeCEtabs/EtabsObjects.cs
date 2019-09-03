5a1dea1a14e28f951f086599f5761c03ea9151b5
Hesham Saleh <32981736+heshamsalehmohammed@users.noreply.github.com>
2019-09-03 09:34:50 +0200

    Update EtabsObjects.cs

--- CeadeCEtabs/EtabsObjects.cs
+++ CeadeCEtabs/EtabsObjects.cs
@@ -9,26 +9,38 @@ namespace EtabsObjects
 {
     public class etabsAnalysisResults
     {
-         Public   int NumberResults = 0;
-         Public   string[] Obj = new string[0];
-         Public   double[] ObjSta = new double[0];
-         Public   string[] Elm = new string[0];
-         Public   double[] ElmSta = new double[0];
-         Public   string[] LoadCase = new string[0];
-         Public   string[] StepType = new string[0];
-         Public   double[] StepNum = new double[0];
-         Public   double[] P = new double[0];
-         Public   double[] V2 = new double[0];
-         Public   double[] V3 = new double[0];
-         Public   double[] T = new double[0];
-         Public   double[] M2 = new double[0];
-         Public   double[] M3 = new double[0];
+         Public   int NumberResults;
+         Public   string[] Obj;
+         Public   double[] ObjSta ;
+         Public   string[] Elm ;
+         Public   double[] ElmSta ;
+         Public   string[] LoadCase ;
+         Public   string[] StepType ;
+         Public   double[] StepNum ;
+         Public   double[] P ;
+         Public   double[] V2 ;
+         Public   double[] V3 ;
+         Public   double[] T ;
+         Public   double[] M2 ;
+         Public   double[] M3 ;
 
-
-
-        public etabsAnalysisResults(mySapModel)
+        public etabsAnalysisResults(ETABS2016.cSapModel mySapModel , string objectName ,eItemTypeElm itemType )
         {
-            mySapModel.Results.FrameForce(objectName, eItemTypeElm.ObjectElm, ref this.NumberResults, ref this.Obj, ref this.ObjSta, ref this.Elm, ref this.ElmSta, ref this.LoadCase, ref this.StepType, ref this.StepNum, ref this.P, ref this.V2, ref this.V3, ref this.T, ref this.M2, ref this.M3);
+         this.NumberResults = 0;
+         this.Obj = new string[0];
+         this.ObjSta = new double[0];
+         this.Elm = new string[0];
+         this.ElmSta = new double[0];
+         this.LoadCase = new string[0];
+         this.StepType = new string[0];
+         this.StepNum = new double[0];
+         this.P = new double[0];
+         this.V2 = new double[0];
+         this.V3 = new double[0];
+         this.T = new double[0];
+         this.M2 = new double[0];
+         this.M3 = new double[0];
+         mySapModel.Results.FrameForce(objectName, itemType, ref this.NumberResults, ref this.Obj, ref this.ObjSta, ref this.Elm, ref this.ElmSta, ref this.LoadCase, ref this.StepType, ref this.StepNum, ref this.P, ref this.V2, ref this.V3, ref this.T, ref this.M2, ref this.M3);
         }
     }
 
